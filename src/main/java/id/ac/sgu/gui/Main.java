f203f9334e07e3a0a4b248f8d9639619ce9e71a1
Michaela Hannah <62824929+Michaela-alt@users.noreply.github.com>
2021-10-20 14:06:31 +0700

    BUILD: added javafx library as dependency and working

---
+++ src/main/java/id/ac/sgu/gui/Main.java
@@ -0,0 +1,28 @@
+package id.ac.sgu.gui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	Button button;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Title of the Window");
+		button = new Button();
+	}
+
+}
