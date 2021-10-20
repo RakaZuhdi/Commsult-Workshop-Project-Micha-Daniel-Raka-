e17352e0279cd963f0d5254fdba262ed912dc86b
RakaZuhdi <68222811+RakaZuhdi@users.noreply.github.com>
2021-10-20 14:36:35 +0700

    Added working wind class

--- src/main/java/World/WorldRunner.java
+++ src/main/java/World/WorldRunner.java
@@ -3,7 +3,7 @@ package World;
 public class WorldRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(new Wind(0, 100).generateValue());
 
 	}
 
