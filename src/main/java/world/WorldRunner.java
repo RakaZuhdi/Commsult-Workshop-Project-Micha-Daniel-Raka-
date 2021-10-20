59fcbd02defc6f3780dc679b5c0ef3968c4687c3
RakaZuhdi <68222811+RakaZuhdi@users.noreply.github.com>
2021-10-20 17:47:18 +0700

    Minor changes

--- src/main/java/world/WorldRunner.java
+++ src/main/java/world/WorldRunner.java
@@ -4,6 +4,7 @@ import sensor.*;
 
 public class WorldRunner {
 	public static void main(String[] args) {
+		System.out.println("Test");
 		StartSensor start = new StartSensor(new Time(0), new Wind(0, 0), new Temperature(0, 0));
 		start.start();
 		System.out.print("sdakfjasl;kdfj;l");
