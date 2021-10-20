056a4d28634e3ec848f5dd6eadcbb3bc43d6d1c1
RakaZuhdi <68222811+RakaZuhdi@users.noreply.github.com>
2021-10-20 14:50:43 +0700

    Added wind class

--- src/main/java/World/Wind.java
+++ src/main/java/World/Wind.java
@@ -6,6 +6,7 @@ public class Wind {
 	private int min;
 
 	public Wind(int min, int max) {
+		this.windSpeed = (int) ((Math.random() * (max - min)) + min);
 		this.max = max;
 		this.min = min;
 	}
@@ -16,7 +17,9 @@ public class Wind {
 	}
 	
 	public void print() {
-		
+		StringBuffer string = new StringBuffer();
+		string.append(this.windSpeed).append(" KM/H");
+		System.out.println(string);
 	}
 	
 	
--- src/main/java/World/WorldRunner.java
+++ src/main/java/World/WorldRunner.java
@@ -1,10 +1,7 @@
 package World;
 
 public class WorldRunner {
-
 	public static void main(String[] args) {
-		System.out.println(new Wind(0, 100).generateValue());
-
+		
 	}
-
 }
